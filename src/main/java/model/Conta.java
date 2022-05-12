dc8451b63a354770249ffb180ad3ddb234bff095
Eduardo <eduardopr@ciandt.com>
2022-05-12 16:16:28 -0300

    [Botcamp-Santander-2022]- Criacao das classes modelos

---
+++ src/main/java/model/Conta.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Conta {
+
+    //Atributos
+    private Long numero;
+    private Double saudo;
+
+    //Getters and Setters
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaudo() {
+        return saudo;
+    }
+
+    public void setSaudo(Double saudo) {
+        this.saudo = saudo;
+    }
+}
---
+++ src/main/java/model/Correntista.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Correntista {
+
+    //Atributos
+    private Integer id;
+    private String cpf;
+    private String nome;
+    private Conta conta;
+
+    // Getters and Setters
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
